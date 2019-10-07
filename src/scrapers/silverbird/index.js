2c67cbd5e0bb2a66a23f75b07e8a49e9488a12dc
Malik Mahmud <almalikmahmud@gmail.com>
2019-10-07 20:10:05 +0100

    Add coming soon scraper for Silverbird Cinemas

--- src/scrapers/silverbird/index.js
+++ src/scrapers/silverbird/index.js
@@ -49,5 +49,28 @@ export const nowShowingMovies = async () => {
 
 
 export const comingSoonMovies = async () => {
-
+  const movies = [];
+  const movieList = await fetch('https://silverbirdcinemas.com/coming-soon/');
+  if (movieList) {
+    const $ = cheerio.load(movieList);
+    const entries = $('article.entry-item');
+    for (let i = 0; i < entries.length; i += 1) {
+      const url = $(entries[i]).find('h2.entry-title > a').attr('href');
+      const entry = await fetch(url);
+      const $$ = cheerio.load(entry);
+      movies.push({
+        title: $$('h1.entry-title > a').text(),
+        synopsis: $$('div.entry-content blockquote p, div.entry-content ol li').text(),
+        starring: $$('ul.info-list li:nth-child(1) span').text(),
+        genre: $$('ul.info-list li:nth-child(3) span a:nth-of-type(1)').text().trim().split(),
+        poster: $$('div.entry-thumb img').attr('src'),
+        trailer: $$('div.media-item > a').attr('href'),
+        release_date: formatReleaseDate($$('ul.info-list li:nth-child(4) > span').text()),
+        rating: $$('div.entry-pg span.pg').text(),
+        runtime: formatRuntime($$('div.entry-pg .duration').text().trim()),
+        now_showing: false,
+      });
+    }
+  }
+  return movies;
 };
